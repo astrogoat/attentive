95fee822bd5ee612a3d9deebe8e122a0a039183c
Kristoffer Tonning <tonning@gmail.com>
2022-08-30 16:28:10 +0200

    Add config comment

--- config/attentive.php
+++ config/attentive.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    /*
+     * If you want to override the automatic injection of views
+     * into some areas of your application so you to include
+     * them yourself then you disable each in this array.
+     */
     'include-frontend-views' => [
         'head' => true,
         'body' => true,
